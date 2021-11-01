2dd57380c3ff89244dca169772eb893018778b16
Issac Teoh <42070824+bluetimer@users.noreply.github.com>
2021-11-01 17:46:05 -0400

    created a table that takes on Json data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,48 @@
 import "./styles.css";
+import data from "./classdata.json";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <table>
+        <tr>
+          <th>
+            <Name text={data[0].FirstName} />
+          </th>
+          <th>
+            <Name text={data[0].LastName} />
+          </th>
+          <th>
+            <Name text={data[0].Pronouns} />
+          </th>
+        </tr>
+        <tr>
+          <td>
+            <Name text={data[1].FirstName} />
+          </td>
+          <td>
+            <Name text={data[1].LastName} />
+          </td>
+          <td>
+            <Name text={data[1].Pronouns} />
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <Name text={data[2].FirstName} />
+          </td>
+          <td>
+            <Name text={data[2].LastName} />
+          </td>
+          <td>
+            <Name text={data[2].Pronouns} />
+          </td>
+        </tr>
+      </table>
     </div>
   );
+}
+
+function Name(props) {
+  return <p> {props.text} </p>;
 }
