a55561d0258fcf5d54f731e3bbeedb557087d3ff
Issac Teoh <42070824+bluetimer@users.noreply.github.com>
2021-11-29 18:20:43 -0500

    edited app.js

--- src/App.js
+++ src/App.js
@@ -1,43 +1,21 @@
 import "./styles.css";
 import data from "./classdata.json";
 
-// function VeryBeautifulNumber(num) {
-//   return (<span> { num } </span>);
-// }
-function displayPerson(first, last) {
-  return;
-  <div>
-    <p> {first}</p>
-    <p>{last}</p>
-  </div>;
-}
-
 export default function App() {
-  let students = [
-    ["Mikkail", "Allen"],
-    ["Justin", "Apupalo"],
-    ["Christopher", "Bonifacio"],
-    ["Angel", "Campoverde"],
-    ["Mahdiya", "Chowdhury"],
-    ["Diego", "Cruz"],
-    ["Heber	Cruz", "Hernandez"],
-    ["Yonatan", "De La Mora"],
-    ["Denny", "Fernandez"]
-  ];
-  //---------------------------------
-  // let array = []
-  // for (let n = 1; n <= 10; n++) {
-  //   array.push(VeryBeautifulNumber(n));
-  // }
-  // let x = 0;
-  // let y = 1;
-  //-------------------------------------------------
-  //look at line 35 and
-  // let numstudents = data.length;
-  // let array = [];
-  // for (let i = 0; i < numstudents; i++) {
-  //   array.push(<Row person={data[i]} />);
-  // }
+  //not used
+  // let students = [
+  //   ["Mikkail", "Allen"],
+  //   ["Justin", "Apupalo"],
+  //   ["Christopher", "Bonifacio"],
+  //   ["Angel", "Campoverde"],
+  //   ["Mahdiya", "Chowdhury"],
+  //   ["Diego", "Cruz"],
+  //   ["Heber	Cruz", "Hernandez"],
+  //   ["Yonatan", "De La Mora"],
+  //   ["Denny", "Fernandez"]
+  // ];
+
+  let numstudents = data.length;
 
   let array = data.map(function (person) {
     return <Row person={person} />;
@@ -45,12 +23,11 @@ export default function App() {
 
   return (
     <div className="App">
-      {console.log(students.length)}
+      {/* {console.log(students.length)} */}
       <h1> Code Nation Class List </h1>
-      {/* <br/><br/><br/><br/><br/> */}
       <table>{array}</table>
       <displayPerson />
-      <p> There are {students.length} of students in this roster.</p>
+      <p> There are {numstudents} of students in this roster.</p>
     </div>
   );
 }
@@ -59,6 +36,20 @@ function Name(props) {
   return <p> {props.text} </p>;
 }
 
+function Pronouns(parameter) {
+  //TODO: Try to figure out how this is not working
+  //codintion statement might be a bit weird
+  let codesnippet;
+  if (parameter === "He/His") {
+    codesnippet = <p class="value1"> {parameter.value} </p>;
+  } else {
+    codesnippet = <p class="value2"> {parameter.value} </p>;
+  }
+  return codesnippet;
+}
+function Year(input) {
+  return <p> {input.interger}</p>;
+}
 function Row(props) {
   return (
     <tr>
@@ -69,14 +60,19 @@ function Row(props) {
         <Name text={props.person.LastName} />
       </td>
       <td>
-        <Name text={props.person.Pronouns} />
+        <Pronouns value={props.person.Pronouns} />
       </td>
       <td>
         <Name text={props.person.School} />
       </td>
       <td>
-        <Name text={props.person.GraduationYear} />
+        <Year interger={props.person.GraduationYear} />
       </td>
     </tr>
   );
 }
+
+//TODO:
+//make a conditional statement when if the pronouns are
+// she/hers underline the following text
+// he/him bold the following text
